22d7f3dfa5128828e5746bb2a3757492429eb4cb
Mikko Mattila <mixu@9ce9c5475ab2>
2019-02-05 13:34:02 +0000

    Code cleanup and ES indexing data type fix.
    Signed-off-by: Mikko Mattila <mixu@9ce9c5475ab2>

--- fitbod2elastic.py
+++ fitbod2elastic.py
@@ -8,23 +8,25 @@ import os
 import csv
 import logging
 import datetime
+import argparse
+
 from dateutil import parser
 from elasticsearch import Elasticsearch
 import gmail_api
 
 logging.basicConfig(level=logging.INFO)
 
-def GetAttachmentsWithQuery(query, tgt_dir):
+def get_attachments(query, tgt_dir):
     """Returns a list of attachments that match the query. Saves files to tgt_dir.
-    
+
     Will overwrite files if they already exist.
     """
-    
+
     service = gmail_api.GetService('credentials.json')
     # Call the Gmail API
     filtered_msgs = gmail_api.ListMessagesMatchingQuery(service, 'me', query)
     logging.debug(filtered_msgs)
-    
+
     attachments = []
     for msg in filtered_msgs:
         attachments.extend(gmail_api.GetAttachments(service, 'me', msg["id"], tgt_dir))
@@ -34,7 +36,7 @@ def GetAttachmentsWithQuery(query, tgt_dir):
 
 def index_to_es(es, index, doc_type, body, doc_id):
     """Indexes the data in 'body' object to Elasticsearch.
-    
+
     Args:
         es: elasticsearch instance
         index: Index name on the ES server
@@ -47,35 +49,7 @@ def index_to_es(es, index, doc_type, body, doc_id):
     if ret["result"] != "created" and ret["result"] != "updated":
         logging.error("Indexing returned %s, expected 'created' or 'updated'" %(ret["result"]))
 
-def main():
-    ES_INDEX = "fitbod-2"
-    ES_DOC_TYPE = "workout_sets"
-    TMP_DIR = os.path.join(os.sep, "tmp", "fitbod2elastic")
-
-    attachments = GetAttachmentsWithQuery('fitbod', TMP_DIR)
-
-    es = Elasticsearch([{'host': '127.0.0.1', 'port': 9201}])
-    
-    for att in attachments:
-        logging.info("Indexing %s" %(att, ))
-        if not att.endswith(".csv"):
-            logging.warning("...skipping due to unknown file type. Expecting CSV.")
-            continue
-    
-        # We can't distinguish similar sets performed on the same day.
-        # So we add row numbers to the CSV, the oldest set being number 1.
-        # The assumption is that old workout data will not be modified.
-        # If the history changes, re-index everything with nr_of_days=0
-        workout_data = csv_to_workout_obj(att, nr_of_days=7)
-        for workout_set in workout_data:
-            index_to_es(es, ES_INDEX, ES_DOC_TYPE, workout_set, workout_set["id"])
-
-    #Cleanup
-    for att in attachments:
-        os.unlink(att)
-
-
-def csv_to_workout_obj(csv_file, nr_of_days=0):
+def csv_to_workout_obj(csv_file, nr_of_days):
     """A CSV is converted to a list of dictionairies."""
 
     #First, we add an ID field.
@@ -96,10 +70,57 @@ def csv_to_workout_obj(csv_file, nr_of_days=0):
         for row in rd:
             #Convert the date to a proper timestamp
             row["timestamp"] = parser.parse(row["timestamp"])
-            if row["timestamp"] > datetime.datetime.now()-datetime.timedelta(days=nr_of_days):
-                obj.append(row)
+            days = datetime.timedelta(days=nr_of_days)
+            date_in_range = row["timestamp"] > datetime.datetime.now() - days
+            if nr_of_days == 0 or date_in_range:
+                formatted_row = {}
+                for field in row.items():
+                    formatted_row[field[0]] = to_float(field[1])
+                obj.append(formatted_row)
     logging.info("Found %s sets within the past %s days" %(len(obj), nr_of_days, ))
     return obj
 
-main()
+def to_float(my_data):
+    """Cast my_data to float if possible, return it unchanged if not."""
 
+    try:
+        return float(my_data)
+    except Exception:
+        return my_data
+
+
+def main():
+    ES_INDEX = "fitbod-workouts"
+    ES_DOC_TYPE = "set"
+    TMP_DIR = os.path.join(os.sep, "tmp", "fitbod2elastic")
+    parser = argparse.ArgumentParser(\
+        description='Get Fitbod CSV from GMail and index it in Elasticsearch.')
+    parser.add_argument('-d', '--days',
+                        help="number of days to index starting from today",
+                        type=int, default=7)
+    args = parser.parse_args()
+
+    attachments = get_attachments('fitbod', TMP_DIR)
+
+    es = Elasticsearch([{'host': '127.0.0.1', 'port': 9201}])
+
+    for att in attachments:
+        logging.info("Indexing %s" %(att, ))
+        if not att.endswith(".csv"):
+            logging.warning("...skipping due to unknown file type. Expecting CSV.")
+            continue
+
+        # We can't distinguish similar sets performed on the same day.
+        # So we add row numbers to the CSV, the oldest set being number 1.
+        # The assumption is that old workout data will not be modified.
+        # If the history changes, re-index everything with nr_of_days=0
+        workout_data = csv_to_workout_obj(att, nr_of_days=args.days)
+        for workout_set in workout_data:
+            index_to_es(es, ES_INDEX, ES_DOC_TYPE, workout_set, workout_set["id"])
+
+    #Cleanup
+    for att in attachments:
+        os.unlink(att)
+
+
+main()
